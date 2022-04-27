067a3b6e007e2ea67e0556e984bc611c5354b757
althaf <althafsubair04@gmail.com>
2022-04-27 14:07:30 +0530

    intro for window completed

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import './intro.css'
+import lamp from './lamp.png'
 
-function Intro() {
+export default function Intro() {
   return (
-    <div>Intro</div>
+    <section className='intro' >
+
+        <div className="upper">
+          <div className="upperleft">
+            <h1>Creative.</h1>
+            <h1>Handmade.</h1>
+            <h1>Crafty.</h1>
+            <p>We come from a place and time forgotten.Our story is one of revival and reconnection to craft</p>
+            <button>Explore</button>
+          </div>
+          <div className="upperright">
+            <div className="boxframe">
+              <img src={lamp} alt="" />
+            </div>
+            </div>
+        </div>
+         
+        <div className="lower">
+          <ul className="footerlist">
+            <li>Facebook</li>
+            <li>Whatsapp</li>
+            <li>Instagram</li>
+          </ul>
+        </div>
+
+    </section>
   )
 }
 
-export default Intro
+
